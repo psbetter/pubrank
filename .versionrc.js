904260031f3fb1c251c9fef44cffa14e54e07878
shun peng <shun.peng@hnluchuan.com>
2024-12-30 20:31:36 +0800

    docs: 代码注释修改

--- .versionrc.js
+++ .versionrc.js
@@ -1,23 +1,23 @@
 module.exports = {
     types: [
       { type: "feat", section: "✨ Features | 新功能" },
-      { type: "fix", section: "🐛 Bug Fixes | 修復 Bug" },
-      { type: "docs", section: "📝 Documentation | 文件變更" },
-      { type: "style", section: "🎨 Styles | 程式碼格式" },
-      { type: "refactor", section: "♻️ Code Refactoring | 程式碼重構" },
+      { type: "fix", section: "🐛 Bug Fixes | 修复 Bug" },
+      { type: "docs", section: "📝 Documentation | 文件变更" },
+      { type: "style", section: "🎨 Styles | 代码格式" },
+      { type: "refactor", section: "♻️ Code Refactoring | 代码重构" },
       {
         type: "perf",
-        section: "⚡ Performance Improvements | 改善效能的程式碼變更",
+        section: "⚡ Performance Improvements | 提升代码性能",
       },
-      { type: "test", section: "✅ Tests | 添加或修正測試" },
+      { type: "test", section: "✅ Tests | 添加或修正测试" },
       { type: "revert", section: "⏪ Revert | 回退先前的提交" },
       {
         type: "build",
-        section: "📦 Build System | 影響構建系統或外部依賴的變更",
+        section: "📦 Build System | 影响构建系統或外部依赖的变更",
       },
       {
         type: "chore",
-        section: "🔧 chore | 其他不修改 src 或測試文件的變更",
+        section: "🔧 chore | 其他不修改 src 或测试文件的变更",
       },
       {
         type: "ci",
